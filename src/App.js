6ee12845d8b08b2ad5da5891c7fc6e7fa900e4cd
mohsenpasdar <66749029+mohsenpasdar@users.noreply.github.com>
2023-01-26 23:58:16 +0330

    front and bach aree connected and working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [message, setMessage] = useState("");
+  const [response, setResponse] = useState("");
+
+  // useEffect(() => {
+  //   console.log(message);
+  //   console.log(response);
+  // })
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch("http://localhost:3001/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ message }),
+    })
+      .then((res) => res.json())
+      .then((data) => setResponse(data.message));
+  };
   return (
-    <div>
-
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <textarea
+          value={message}
+          onChange={(e) => {
+            setMessage(e.target.value);
+          }}
+        ></textarea>
+        <button type="submit">Submit</button>
+      </form>
+      <div>{response}</div>
     </div>
   );
-}
+};
 
 export default App;
